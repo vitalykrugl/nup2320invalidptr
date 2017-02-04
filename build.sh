773d3afd8e069a8963116b2121b04f199e02b8bd
Vitaly Kruglikov <vitaly.krugl.numenta@gmail.com>
2017-02-03 17:57:17 -0800

    More flags from debug build of nupic.core. Still can't reproduce invalid pointer.

--- build.sh
+++ build.sh
@@ -1,4 +1,17 @@
-g++ -fPIC dll.cpp -I./ -shared -o libdll.so || exit
 
-g++ app.cpp -I./ -L./ -ldll -Wl,-rpath=\$ORIGIN -o app || exit
+SHARED_CXX_FLAGS="-g -Og   -fdiagnostics-show-option -m64 -fPIC   -std=c++11  -Werror -Wextra -Wreturn-type -Wunused -Wno-unused-variable -Wno-unused-parameter -Wno-missing-field-initializers -Wall"
+
+SHARED_LINK_FLAGS="-O0  -m64    -Wl,--allow-shlib-undefined"
+
+SHARED_LINK_LIBS="-lpthread -ldl"
+
+APP_CXX_FLAGS="-g -Og   -fdiagnostics-show-option -m64 -fPIC -std=c++11  -Werror -Wextra -Wreturn-type -Wunused -Wno-unused-variable -Wno-unused-parameter -Wno-missing-field-initializers -Wall"
+
+APP_LINK_FLAGS="-O0  -m64   -Wl,--no-undefined -Wl,-rpath=\$ORIGIN"
+
+APP_LINK_LIBS="-l:/usr/lib/x86_64-linux-gnu/libpython2.7.so -lpthread -ldl"
+
+g++ ${SHARED_CXX_FLAGS} dll.cpp -I./ ${SHARED_LINK_LIBS} ${SHARED_LINK_FLAGS} -shared -o libdll.so || exit
+
+g++ ${APP_CXX_FLAGS} app.cpp -I./ -L./ -ldll ${APP_LINK_LIBS} ${APP_LINK_FLAGS} -o app || exit
 
